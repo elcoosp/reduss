4e064afc2aeaa85b729687534aa15ea0c30e90ff
Elcoosp <elcoosp@gmail.com>
2018-05-11 18:39:54 +0200

    docs(sumOnly): Add doc

--- src/sumOnly.js
+++ src/sumOnly.js
@@ -1,2 +1,8 @@
-module.exports = condition => numbers =>
+/**
+ * @param {(number) => boolean} condition A function returning true if the current iterated number must be summed, otherwise false
+ * @return {(numbers: Array<number>) => number} The sum of the numbers passing the condition
+ */
+const sumOnly = condition => numbers =>
   numbers.reduce((acc, n) => (condition(n) ? acc + n : acc), 0)
+
+module.exports = sumOnly
