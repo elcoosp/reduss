d4c874bebbe102fc5e0b0e21ada7279abdfa363d
Elcoosp <elcoosp@gmail.com>
2018-05-13 22:40:37 +0200

    refactor(sumOnly): Uncurry
    sumOnly is no longer a curried fucntion

--- src/sumOnly.js
+++ src/sumOnly.js
@@ -1,7 +1,7 @@
 /**
 sumOnly(condition: Number => Boolean) => (numbers: [Number...]) => Number
 */
-const sumOnly = condition => numbers =>
+const sumOnly = (condition, numbers) =>
   numbers.reduce((acc, n) => (condition(n) ? acc + n : acc), 0)
 
 module.exports = sumOnly
