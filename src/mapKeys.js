cbe0cb94b9c23f0d60427eb12a2d3c7d79e0da4c
Elcoosp <elcoosp@gmail.com>
2018-05-13 22:36:49 +0200

    refactor(mapKeys): Uncurry function
    BREAKING CHANGE: mapKeys is no more a curried function

--- src/mapKeys.js
+++ src/mapKeys.js
@@ -5,7 +5,7 @@
 
  mapKeys(mappers: mappers) => (objectToMap: Object) => Object
  */
-const mapKeys = mappers => objectToMap =>
+const mapKeys = (mappers, objectToMap) =>
   Object.entries(objectToMap).reduce((acc, [key, val]) => {
     acc[key] = mappers[key] ? mappers[key](val) : val
     return acc
