bf25c6006a737a9848af1d287689484b31cb2558
Elcoosp <elcoosp@gmail.com>
2018-05-14 01:08:56 +0200

    fix(atPathSet): Return the content of the current iterated prop if there is one, instead of a plain

--- src/atPathSet.js
+++ src/atPathSet.js
@@ -3,12 +3,17 @@
  * @param {*} data The data to set
  * @param {Object}  [baseObject=Object] The base object, serve as the initial accumulator, default {}
  * @returns {Object} A new object with the path provided equal to data, and optionnaly any values present in baseObject
+ * @example
+ * //returns {
+ * some:{ prop: { one: 1, two: 2 } }
+ *}
+ *atPathSet('some.prop.one', 1, atPathSet('some.prop.two', 2))
  */
 
 const atPathSet = (path, data, baseObject = {}) =>
   path.split('.').reduce((acc, prop, i, a) => {
     const isLastProp = i === a.length - 1
-    acc[prop] = isLastProp ? data : {}
+    acc[prop] = isLastProp ? data : { ...acc[prop] }
     return isLastProp ? baseObject : acc[prop]
   }, baseObject)
 
