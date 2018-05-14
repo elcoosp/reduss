0e916d04530ab71c8a17ee97292cf0d64ceae501
Elcoosp <elcoosp@gmail.com>
2018-05-14 14:44:34 +0200

    refactor(sumOnly): Use reduceIf

--- src/sumOnly.js
+++ src/sumOnly.js
@@ -1,10 +1,14 @@
+const reduceIf = require('./reduceIf')
 /**
  *
  * @param {function} condition Retun true if the number can be summed
  * @param {Array<number>} numbers The array of numbers to sum
  * @returns {number} The sum of matching numbers
+ * @example
+ * // returns 6
+ * sumOnly(x => x <= 3, [1,2,3,4])
  */
 const sumOnly = (condition, numbers) =>
-  numbers.reduce((acc, n) => (condition(n) ? acc + n : acc), 0)
+  reduceIf(condition, (acc, v) => acc + v, numbers, 0)
 
 module.exports = sumOnly
